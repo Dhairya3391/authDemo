f8c0fd9fc86a892d32c4daba2727e566dfe3cd46
Dhairya3391 <dhairyaadroja3391@gmail.com>
2025-12-19 12:44:41 +0530

    ss

---
+++ docs/.docusaurus/routes.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/markdown-page',
+    component: ComponentCreator('/markdown-page', '3d7'),
+    exact: true
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', 'e5f'),
+    exact: true
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', '362'),
+    routes: [
+      {
+        path: '/',
+        component: ComponentCreator('/', 'a0a'),
+        routes: [
+          {
+            path: '/',
+            component: ComponentCreator('/', '05e'),
+            routes: [
+              {
+                path: '/backend-dotnet',
+                component: ComponentCreator('/backend-dotnet', '44e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/deployment',
+                component: ComponentCreator('/deployment', 'a8a'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/frontend-vite',
+                component: ComponentCreator('/frontend-vite', '3cf'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/google-cloud-setup',
+                component: ComponentCreator('/google-cloud-setup', '672'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/running-locally',
+                component: ComponentCreator('/running-locally', 'f41'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/troubleshooting',
+                component: ComponentCreator('/troubleshooting', '810'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/',
+                component: ComponentCreator('/', 'fc9'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
